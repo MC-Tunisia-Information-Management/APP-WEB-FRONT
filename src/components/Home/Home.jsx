4c01069ccc4e43c4aaf55a6a06f6ad95b3b14585
RayenBelkh <belkhouja.rayen@aiesec.net>
2024-04-21 21:20:22 +0200

    home page kemlet b 90%

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './home.css'
 import NavBar from '../NavBar/NavBar'
-import homePageImage from '../../assests/home-page-image.png'
+import homePageImage from '../../assests/home-page-image-finish.png'
 import Footer from '../Footer/Footer'
 import cursor from '../../assests/cursor.png'
 import { AiFillFacebook } from "react-icons/ai";
@@ -31,18 +31,21 @@ function Home() {
                 </div>
               </div>
               <div className='flex-center home-page-image-container'>
-                <img src={homePageImage} alt="" />
+                <img className='home-page-image' src={homePageImage} alt="" />
               </div>
             </div>
             <div className='search'>
               <div></div>
               <div></div>
-              <div>
+              <div className='search-container'>
                 <div>
-                  <div>CAN'T FIND SOMETHIN?</div>
-                  <div>TRY SEARCHING FOR IT BELOW!</div>
+                  <div className='konnect1'>CAN'T FIND SOMETHING?</div>
+                  <div className='konnect2'>TRY SEARCHING FOR IT BELOW!</div>
                 </div>
-                <div></div>
+                <div className='search-input-container'>
+                  <img src="" alt="" />
+                  <input className='search-input' type="text" placeholder='Search'/>
+                </div>
               </div>
             </div>
             <div className='flex-horizontal social-media'>
@@ -64,9 +67,25 @@ function Home() {
                     <div className='social-media-links-text'>AIESEC in Tunisia</div>
                   </div>                
                 </div>
+                <div className='flex-horizontal social-media-links'>        
+                  <div className='logoo'>
+                    <AiFillInstagram  className='social-media-links-logo'/>
+                  </div>
+                  <div className='texxt'>
+                    <div className='social-media-links-text'>aiesecintunisia</div>
+                  </div>                
+                </div>
+                <div className='flex-horizontal social-media-links'>        
+                  <div className='logoo'>
+                    <AiFillLinkedin  className='social-media-links-logo'/>
+                  </div>
+                  <div className='texxt'>
+                    <div className='social-media-links-text'>AIESEC Tunisia</div>
+                  </div>                
+                </div>
               </div>
               <div>
-                <img src={cursor} alt="" />
+                <img className='cursor-home-page' src={cursor} alt="" />
               </div>
             </div>  
             <Footer/>
